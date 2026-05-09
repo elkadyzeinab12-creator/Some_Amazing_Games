0312f8929bfc48ea9d153a633831cd36eb175836
Mona_Ahmed_Eid <monaahmedeidramadan@gmail.com>
2026-05-09 04:06:29 -0700

    Add base in conect 4

--- Connect4.cpp
+++ Connect4.cpp
@@ -3,6 +3,33 @@
 #include "utils.h"
 #include "colors.h"
 
-
 using namespace std;
 
+void initialVal(char board[7][7]) {
+    for (int i = 0; i < 7; i++) {
+        for (int j = 0; j < 7; j++) {
+            board[i][j] = ' ';
+        }
+    }
+}
+
+void printBoard(char board[7][7]) {
+    cout << "1 + 2 + 3 + 4 + 5 + 6 + 7" << '\n';
+    for (int i = 0; i < 7; i++) {
+        for (int j = 0; j < 7; j++) {
+            cout << board[i][j];
+            if (j != 6)cout << " | ";
+        }
+        if (i != 6)cout << "\n--+---+---+---+---+---+--\n";
+    }
+}
+
+bool checkRow(char board[7][7], char c, int col) {
+    for (int i = 6; i >= 0; i--) {
+        if (board[i][col] == ' ') {
+            board[i][col] = c;
+            return true;
+        }
+    }
+    return false;
+}
--- Connect4.h
+++ Connect4.h
@@ -4,10 +4,13 @@
 #include "utils.h"
 #include "colors.h"
 
-bool check_cell(char arr[7][7],int column, int row);
-
 bool winner(char arr[7][7]);
 
 bool grid(char arr[7][7]);
 
+void initialVal(char board[7][7]);
+
+void printBoard(char board[7][7]);
+
+bool checkCell(char board[7][7], char c, int col);
 #endif //SOME_AMAZING_GAMES_CONNECT4_H
