fd6530f4ed48101df845a2472b0a032603434a0f
check <firevvv811@gmail.com>
2026-05-09 12:27:40 +0300

    add function check cell

--- Connect4.cpp
+++ Connect4.cpp
@@ -6,3 +6,21 @@
 
 using namespace std;
 
+#include <bits/stdc++.h>
+#include "utils.h"
+#include "colors.h"
+
+bool check(int &row, int &col, char board[7][7])
+{
+    if (row < 0 || row > 6 || col < 0 || col > 6) {
+        cout << "Outside the game area";
+        return false;
+    }
+
+    if (board[row][col] != ' ') {
+        cout << "This position is already occupied";
+        return false;
+    }
+
+    return true;
+}
