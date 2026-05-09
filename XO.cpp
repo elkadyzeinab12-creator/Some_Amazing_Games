f6ac5d64800e5cc6c40eb3180fc856e03d3317c2
Malak <malakheshamhesham971@gmail.com>
2026-05-09 04:38:04 +0300

    Adding check function

--- XO.cpp
+++ XO.cpp
@@ -1,5 +1,39 @@
 #include <bits/stdc++.h>
-#include "utils.h"
-#include "colors.h"
 using namespace std;
+bool chckWin(char board[4][4] ,char player) {
+    for(int i = 0 ; i < 4 ; i++) {
+        if(board[i][0] == player &&
+            board[i][1] == player &&
+            board[i][2] == player &&
+            board[i][3] == player)
+            return true;
+    }
+    for(int j = 0 ; j < 4 ; j++) {
+        if(board[0][j] == player &&
+            board[1][j] == player &&
+            board[2][j] == player &&
+            board[3][j] == player)
+            return true;
+    }
+    if(board[0][0] == player &&
+        board[1][1] == player &&
+        board[2][2] == player &&
+        board[3][3] == player)
+        return true;
 
+    if(board[0][3] == player &&
+      board[1][2] == player &&
+      board[2][1] == player &&
+      board[3][0] == player)
+        return true;
+    return false;
+}
+bool checkDraw(char board[4][4]) {
+    for(int i = 0 ; i < 4 ; i++) {
+        for(int j = 0 ; j < 4 ; j++) {
+            if (board[i][j] == '-')
+                return false;
+        }
+    }
+    return true;
+}
