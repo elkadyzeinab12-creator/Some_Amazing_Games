e3bb8267957f7b501cb117e02b241159bc6195e9
Reem Hasan <reem8073@gmail.com>
2026-05-09 14:13:47 +0300

    the full code of the xo game2

--- XO.cpp
+++ XO.cpp
@@ -2,6 +2,7 @@
 #include "utils.h"
 #include "colors.h"
 #define PURPLE "\033[35m"
+#define MAGENTAd "\033[35m"
 #define CYAN   "\033[36m"
 #define RED    "\033[1;31m"
 #define RESET  "\033[0m"
@@ -60,170 +61,146 @@ bool checkDraw(char board[4][4]) {
     }
     return true;
 }
-
-void GameOver() {
-    char boardgame[4][4];
-	cout << "Enter the name of player 1: \n";
-	string player1;
-	cin >> player1;
-	cout << "Enter the name of player 2: \n";
-	string player2;
-	cin >> player2;
-    cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::::\n";
-	cout << "Welcome " <<" "<<player1<<" "<< " and "<<" "<< player2<<" "<< " to the BROAD GAME\n";
-	cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::::\n";
-    cout << "========================================================================================================================\n";
-    cout << "The rules of the game are simple:\n";
-	cout << "1. The game is played on a 4x4 grid.\n";
-	cout << "2. Each player takes turns to place their symbol (X or O) on the grid.\n";
-	cout << "3. The first player to get 4 of their symbols in a row as follows:\n "<<
-         "(horizontally, vertically, or diagonally) wins the game.\n";
-	cout << "4. If all the cells are filled and no player has 4 in a row, the game is a draw.\n";
-	cout << "Let's start the game!\n";
-    cout << "========================================================================================================================\n";
-    cout << ":::::::::::::\n";
-    cout << "BROAD GAME\n";
-	cout << ":::::::::::::\n";
+void printBoard(char bgame[4][4]) {
     for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            boardgame[i][j] = '-';
-        }
-    }
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            cout << boardgame[i][j] << " | ";
-        }
-        cout << endl;
-        if (i != 3)
-            cout << "----------------\n " ;
 
+        for (int j = 0; j < 4; j++) {
+
+            if (bgame[i][j] == 'X')
+                cout << CYAN << 'X' << RESET << " ";
+
+            else if (bgame[i][j] == 'O')
+                cout << PURPLE << 'O' << RESET << " ";
+
+            else
+                cout << bgame[i][j] << " ";
+        }
+
+        cout << "\n";
     }
-    char y, d;
+}
+void rowcol(int &r, int &c, string player,char h[4][4]) {
     while (true) {
-        cout << ":::::::::::::::::\n";
-        cout << "select X,x or O,o \n ";
-        cout << ":::::::::::::::::\n";
-        cout << player1 << ":\n";
-        cin >> y;
-        cout << player2 << ":\n";
-        cin >> d;
-
-        if (!((y == 'X' || y == 'O' || y == 'x' || y == 'o') && (d == 'X' || d == 'O' || d == 'x' || d == 'o'))) {
-            cout<<RED<< "wrong input!\n"<<RESET;
-            cout<<RED<<"please try again.\n"<<RESET;
-            cout << "===============================\n";
-        }
-        else if (tolower(y) ==tolower(d)) {
-            cout<<RED<< "the players can't choose the same symbols.\n"<<RESET;
+        cout << player << " Enter row and column numbers : \n";
+        cout << "_________________________________________\n";
+        cout << "Enter row number: \n";
+        cin >> r;
+        cout << "Enter column number: \n";
+        cin >> c;
+        if (!check(r, c, h)) {
             cout<<RED<< "please try again.\n"<<RESET;
-            cout << "===============================\n";
+            cout << "=================================\n";
         }
         else
             break;
     }
+}
+
+
+
+    void GameOver() {
+        char boardgame[4][4];
+        cout << "Enter the name of player 1: \n";
+        string player1;
+        cin >> player1;
+        cout << "Enter the name of player 2: \n";
+        string player2;
+        cin >> player2;
+        cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::::\n";
+        cout << "Welcome " <<" "<<player1<<" "<< " and "<<" "<< player2<<" "<< " to the BROAD GAME\n";
+        cout << "::::::::::::::::::::::::::::::::::::::::::::::::::::::\n";
+        cout << "========================================================================================================================\n";
+        cout << "The rules of the game are simple:\n";
+        cout << "1. The game is played on a 4x4 grid.\n";
+        cout << "2. Each player takes turns to place their symbol (X or O) on the grid.\n";
+        cout << "3. The first player to get 4 of their symbols in a row as follows:\n "<<
+             "(horizontally, vertically, or diagonally) wins the game.\n";
+        cout << "4. If all the cells are filled and no player has 4 in a row, the game is a draw.\n";
+        cout << "Let's start the game!\n";
+        cout << "========================================================================================================================\n";
+        cout << ":::::::::::::\n";
+        cout << "BOARD GAME\n";
+        cout << ":::::::::::::\n";
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                boardgame[i][j] = '-';
+            }
+        }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                cout << boardgame[i][j] << " | ";
+            }
+            cout << endl;
+            if (i != 3)
+                cout << "----------------\n " ;
+
+        }
+        char y, d;
+        while (true) {
+            cout << ":::::::::::::::::\n";
+            cout << "select X,x or O,o \n ";
+            cout << ":::::::::::::::::\n";
+            cout << player1 << ":\n";
+            cin >> y;
+            cout << player2 << ":\n";
+            cin >> d;
+
+            if (!((y == 'X' || y == 'O' || y == 'x' || y == 'o') && (d == 'X' || d == 'O' || d == 'x' || d == 'o'))) {
+                cout<<RED<< "wrong input!\n"<<RESET;
+                cout<<RED<<"please try again.\n"<<RESET;
+                cout << "===============================\n";
+            }
+            else if (tolower(y) ==tolower(d)) {
+                cout<<RED<< "the players can't choose the same symbols.\n"<<RESET;
+                cout<<RED<< "please try again.\n"<<RESET;
+                cout << "===============================\n";
+            }
+            else
+                break;
+        }
         cout << "=================================\n";
 
-         while(true) {
+        while (true) {
             int row1, col1;
-            while (true) {
-                cout << player1 << " Enter row and column numbers : \n";
-                cout << "_________________________________________\n";
-                cout << "Enter row number: \n ";
-                cin >> row1;
-                cout << "Enter column number: \n";
-                cin >> col1;
-                if (!check(row1, col1, boardgame)) {
-                    cout<<RED<< "please try again.\n"<<RESET;
-                    cout << "=================================\n";
-                }
-                else
-                    break;
-            }
+    rowcol(row1,col1,player1,boardgame);
             boardgame[row1][col1] = y;
 
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    if (i == row1 && j == col1) {
-                        boardgame[i][j] = y;
-                    }
-                }
-            }
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    if (boardgame[i][j] == 'X')
-                        cout << CYAN << 'X' << RESET << " ";
 
-                    else if (boardgame[i][j] == 'O')
-                        cout << PURPLE << 'O' << RESET << " ";
+            printBoard(boardgame);
 
-                    else
-                        cout << boardgame[i][j] << " ";
-                }
-                cout << " \n";
-            }
             if (checkWin(y,boardgame)) {
-                cout << player1<<" "<< "is the winner\n";
+                cout<<"++++++++++++++++++++++++++++++++++++++++\n";
+                cout<<MAGENTAd<<"         "<< player1<<" "<< "is the winner\n"<<RESET;
+                cout<<"++++++++++++++++++++++++++++++++++++++++\n";
+
                 cout<<RED<<"Good try,"<<player2<<"! You lost this round.\n"<<RESET;
                 break;
             }
-            if (checkDraw( boardgame)==1) {
+            if (checkDraw( boardgame)) {
                 cout<<RED<< "it's draw! there is no winner."<<RESET;
                 break;
             }
 
             int row2, col2;
-            while (true) {
-                cout << player2 << " Enter row and column numbers : \n";
-                cout << "_________________________________________\n";
-                cout << "Enter row number: \n";
-                cin >> row2;
-                cout << "Enter column number: \n";
-                cin >> col2;
-                if (!check(row2, col2, boardgame)) {
-                    cout<<RED<< "please try again.\n"<<RESET;
-                    cout << "=================================\n";
-                }
-                else
-                    break;
-            }
+            rowcol(row2,col2,player2,boardgame);
             boardgame[row2][col2] = d;
 
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    if (i == row2 && j == col2) {
-                        boardgame[i][j] = d;
-                    }
-                    if (i == row1 && j == col1) {
-                        boardgame[i][j] = y;
-                    }
-                }
-            }
 
-            for (int i = 0; i < 4; i++) {
-                for (int j = 0; j < 4; j++) {
-                    if (boardgame[i][j] == 'X')
-                        cout << CYAN << 'X' << RESET << " ";
-
-                    else if (boardgame[i][j] == 'O')
-                        cout << PURPLE << 'O' << RESET << " ";
-
-                    else
-                        cout << boardgame[i][j] << " ";
-                }
-                cout <<" \n ";
-            }
+            printBoard(boardgame);
 
             if (checkWin(d,boardgame)) {
-                cout << player2 << "is the winner\n";
+                cout<<"++++++++++++++++++++++++++++++++++++++++\n";
+                cout<<MAGENTAd<<"          "<< player2<<" "<< "is the winner\n"<<RESET;
+                cout<<"++++++++++++++++++++++++++++++++++++++++\n";
                 cout<<RED<<"Good try,"<<player1<<"! You lost this round.\n"<<RESET;
 
                 break;
             }
-             if (checkDraw(boardgame)) {
+            if (checkDraw(boardgame)) {
                 cout<<RED<< "it's draw! there is no winner."<<RESET;
                 break;
             }
 
         }
 
-}
+    }
